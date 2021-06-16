8bc0f1554e922cf123499c5007e65d8ebf311da1
HOME-2700 <metalhwal@gmail.com>
2021-06-16 21:17:03 +0900

    last

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 from:  [Zoom Clone in Python](https://www.youtube.com/watch?v=bJOvYgSqrOs) 
 
+If you're having trouble with pyaudio while installing vidstream try this: 1. pip install pipwin 2. pipwin install pyaudio 3. pip install vidstream
--- main2.py
+++ main2.py
@@ -11,8 +11,8 @@ local_ip_address = socket.gethostbyname(socket.gethostname()) # 192.168.244.1
 # public_ip_address = requests.get('https://yoursite.com/app').text
 print(local_ip_address)
 
-server = StreamingServer(local_ip_address, 9999)
-receiver = AudioReceiver(local_ip_address, 8888)
+server = StreamingServer(local_ip_address, 7777)
+receiver = AudioReceiver(local_ip_address, 6666)
 
 def start_listening():
   t1 = threading.Thread(target=server.start_server)
@@ -21,17 +21,17 @@ def start_listening():
   t2.start()
 
 def start_camera_stream():
-  camera_client = CameraClient(text_target_ip.get(1.0, 'end-1c'), 7777)
+  camera_client = CameraClient(text_target_ip.get(1.0, 'end-1c'), 9999)
   t3 = threading.Thread(target=camera_client.start_stream)
   t3.start()
 
 def start_screen_sharing():
-  screen_client = ScreenShareClient(text_target_ip.get(1.0, 'end-1c'), 7777)
+  screen_client = ScreenShareClient(text_target_ip.get(1.0, 'end-1c'), 9999)
   t4 = threading.Thread(target=screen_client.start_stream)
   t4.start()
 
 def start_audio_stream():
-  audio_sender = AudioSender(text_target_ip.get(1.0, 'end-1c'), 6666)
+  audio_sender = AudioSender(text_target_ip.get(1.0, 'end-1c'), 8888)
   t5 = threading.Thread(target=audio_sender.start_stream)
   t5.start()
 
